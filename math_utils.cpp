5697591fdff954f3f3cf6a51fd6b774c70bc3cad
PranjalThakur01 <thakur.akkii0123@gmail.com>
2026-03-15 03:31:01 +0530

    Correct inner loop index in twoSum function
    Fix the inner loop index to start from i + 1 to avoid duplicate pairs.

--- math_utils.cpp
+++ math_utils.cpp
@@ -1,1 +1,14 @@
+#include <vector>
+using namespace std;
 
+vector<int> twoSum(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        // BUG: j should start from i + 1, not 0
+        for (int j = 0; j < nums.size(); j++) { 
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
