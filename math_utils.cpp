b9eaf37969c75366d55a497e8a27825ea9c34a76
PranjalThakur01 <thakur.akkii0123@gmail.com>
2026-03-15 12:01:30 +0530

    Fix bug in twoSum function inner loop index

--- math_utils.cpp
+++ math_utils.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> twoSum(vector<int>& nums, int target) {
     for (int i = 0; i < nums.size(); i++) {
         // BUG: j should start from i + 1, not 0
-        for (int j = 0; j < nums.size(); j++) { 
+        for (int j = I; j < nums.size(); j++) { 
             if (nums[i] + nums[j] == target) {
                 return {i, j};
             }
